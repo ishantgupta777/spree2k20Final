9cefc2be26c916d1b64ced76a6637a211b6756e6
ishant <ishantgupta777@gmail.com>
2019-12-14 23:40:36 +0530

    website complete with testing and git dir move to parent and all commit changes

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const app = express()
+const {
+    google
+} = require('googleapis');
+const keys = require('./key.json');
+
+app.use(express.static('./client/build'))
+app.use(express.json())
+
+var sheet = google.sheets('v4');
+
+
+const port = 5000 || process.env.PORT
+
+app.post('/register', (req, res) => {
+    const {
+        name,
+        email,
+        phone,
+        city,
+        college,
+        year,
+        gender,
+        selectedSports
+    } = req.body
+    authorize(async function (authClient) {
+        var request = {
+            spreadsheetId: '11sUzVRd1EBhqrcAUUbOHwCAvSzG2pbJaufM1bk2z6L8',
+            range: 'A1',
+            auth: authClient,
+            valueInputOption: 'RAW',
+            insertDataOption: 'INSERT_ROWS',
+            resource: {
+                values: [
+                    [name,
+                        email,
+                        phone,
+                        city,
+                        college,
+                        year,
+                        gender,
+                        ...selectedSports
+                    ]
+                ]
+            }
+        };
+
+        await sheet.spreadsheets.values.append(request, function (err, response) {
+            if (err) {
+                console.error(err);
+                res.status(500).send()
+            }
+
+            res.status(200).send()
+        });
+    });
+
+})
+
+
+
+async function authorize(callback) {
+    var authClient = new google.auth.JWT(keys.client_email, null, keys.private_key, [
+        'https://www.googleapis.com/auth/spreadsheets'
+    ]);
+    if (authClient == null) {
+        console.log('authentication failed');
+        return;
+    }
+    callback(authClient);
+}
+
+app.listen(port, () => console.log(`listening on port ${port}`))

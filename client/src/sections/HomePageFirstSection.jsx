b8bd086d7707fe9ad62a940a03cc9329292feef6
ishant <ishantgupta777@gmail.com>
2019-12-21 21:20:28 +0530

    devsoc credeits added

--- client/src/sections/HomePageFirstSection.jsx
+++ client/src/sections/HomePageFirstSection.jsx
@@ -38,6 +38,9 @@ const HomePageFirstSection = () => {
 						</a>
 					</div>
 				</div>
+				<div className="credits">
+					<h2>Made with &#9829; by DevSoc </h2>
+				</div>
 			</section>
 		</div>
 	);
